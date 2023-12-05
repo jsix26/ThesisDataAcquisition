d7a94b055a866a343057f140b6fa984e5e98e257
Junyl Madca <junyl.madca@g.msuiit.edu.ph>
2023-12-05 17:26:48 +0800

    12/5/2023

--- app.js
+++ app.js
@@ -6,28 +6,34 @@ var xbeeAPI = new xbee_api.XBeeAPI({
   api_mode: 1
 });
 
-var serialport = new SerialPort({ path: "COM4", baudRate: 2400});
+var serialport = new SerialPort({ path: "COM10", baudRate: 2400});
 
 serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
 
+xbeeAPI.parser.on("data", function(frame) {
+  const dataString = frame.data.toString();
+  const dataArray = dataString.split(',');
+  const deviceID = dataArray[1];
+  console.log(deviceID, '>>', deviceValues(dataArray));
+});
+
 /*
 serialport.on("open", function() {
   var frame_obj = {
-    type: 0x00, 
+    type: 0x00,   
     id: 0x00,
     destination64: "0013a200421be22b",
     data: "$,0,0,0,3,12,2023,@" 
   }
   xbeeAPI.builder.write(frame_obj);
 });
-*/
+
 
 xbeeAPI.parser.on("data", function(frame) {
 	console.log(">>", frame.data.toString('utf8'));
 });
 
-/*
 const { SerialPort } = require("serialport");
 const { ReadlineParser } = require("@serialport/parser-readline");
 const serialport = new SerialPort({ path: "COM4", baudRate: 2400});
@@ -44,102 +50,56 @@ read.on("data", (data) => {
   console.log(deviceValues(dataString));
 });
 
-
-function deviceValues(dataStr) {
-  const dataArray = dataStr.split(',');
-  const deviceID = parseFloat(dataArray[1]);
-  switch(deviceID) {
+*/
+function deviceValues(dataArr) {
+  const id = parseFloat(dataArr[1]);
+  switch(id) {
     case 1:
-      console.log("1");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 2:
-      console.log("2");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 3:
-      console.log("3");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 4:
-      console.log("4");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 5:
-      console.log("5");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 6:
-      console.log("6");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 7:
-      console.log("7");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 8:
-      console.log("8");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 9:
-      console.log("9");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 10:
-      console.log("10");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 11:
-      console.log("11");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 12:
-      console.log("12");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 13:
-      console.log("13");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 14:
-      console.log("14");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 15:
-      console.log("15");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 16:
-      console.log("16");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 17:
-      console.log("17");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 18:
-      console.log("18");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 19:
-      console.log("19");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     case 20:
-      console.log("20");
-      return getVrms(dataArray);
+      return getVrms(dataArr);
     default: 
-      return "Error. Putol ang string.";
+      return "Device ID not from 1 to 20.";
   }
 }
 
 function getVrms(dataArr) {
-  const vrms = parseFloat(dataArr[2]);
+  var vrms = dataArr[2];
   return vrms;  
 }
-
-function getIrms(dataArr) {
-  const irms = parseFloat(dataArr[3]);
-  return irms;  
-}
-
-
-// Create an interface for reading user input from the console
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-// Prompt the user for commands and send them to COM4
-rl.question("Update date and time (if needed): \n", (command) => {
-    serialport.write(command, (err) => {
-      if (err) {
-        console.error("Error:", err);
-      } else {
-        console.log("Date and time updated:", command);
-      }
-      // Close the readline interface and the serial port
-      rl.close();
-    });
-});
-*/

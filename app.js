eb8116ba178a04f77b2e12c7e215ce285e405626
jsix26 <152432476+jsix26@users.noreply.github.com>
2024-05-16 21:03:15 +0800

    before defense

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var SerialPort = require('serialport').SerialPort;
 var xbee_api = require('xbee-api');
 const mysql = require('mysql2');
+/*
 const connection = mysql.createConnection({
   host     : '127.0.0.1',
   user     : 'root',
   password : 'admin',
   database : 'smartsensedatabase'
 });
+
  
+/*
 connection.connect(function(err) {
   if (err) {
     console.error('Error connecting: ' + err.stack);
@@ -16,19 +19,24 @@ connection.connect(function(err) {
  
   console.log('Connected as id ' + connection.threadId);
 });
+*/
 
 const express = require('express');
+const path = require('path'); 
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
+const io = new Server(server,{
+  port:3000
+});
+
 
 var xbeeAPI = new xbee_api.XBeeAPI({
   api_mode: 2
 });
 
-var serialport = new SerialPort({ path: 'COM14', baudRate: 2400});
+var serialport = new SerialPort({ path: 'COM5', baudRate: 2400});
 
 serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
@@ -41,10 +49,12 @@ xbeeAPI.parser.on('data', function(frame) {
   const now = new Date();
   const formattedDate = now.toLocaleString();
   const deviceData = (deviceID + ', ' + deviceVrms + ', ' + formattedDate);
+  console.log(deviceData);
   switch (deviceID) {
     case 1:
       const deviceVrms1 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms1, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 1.");
@@ -57,10 +67,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;
     case 2:
       const deviceVrms2 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms2, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 2.");
@@ -73,10 +85,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;
     case 3:
       const deviceVrms3 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms3, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 3.");
@@ -89,10 +103,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;    
     case 4:
       const deviceVrms4 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms4, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 4.");
@@ -105,10 +121,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;
     case 5:
       const deviceVrms5 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms5, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 5.");
@@ -121,10 +139,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;
     case 6:
       const deviceVrms6 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms6, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 6.");
@@ -137,10 +157,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;
     case 7:
       const deviceVrms7 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms7, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 7.");
@@ -153,10 +175,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;
     case 8:
       const deviceVrms8 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms8, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 8.");
@@ -169,10 +193,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;      
     case 9:
       const deviceVrms9 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms9, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 9.");
@@ -185,10 +211,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;
     case 10:
       const deviceVrms10 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms10, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 10.");
@@ -201,10 +229,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;     
     case 11:
       const deviceVrms11 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms11, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 11.");
@@ -217,10 +247,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;    
     case 12:
       const deviceVrms12 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms12, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 12.");
@@ -233,10 +265,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;    
     case 13:
       const deviceVrms13 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms13, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 13.");
@@ -249,10 +283,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;     
     case 14:
       const deviceVrms14 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms14, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 14.");
@@ -265,10 +301,12 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break;    
     case 15:
       const deviceVrms15 = parseFloat(dataArray[2]);
       io.emit('data', {deviceID, deviceVrms15, formattedDate});
+      /*
       connection.connect(function(err) {
         if(err) throw err;
         console.log("Reading device 15.");
@@ -281,15 +319,20 @@ xbeeAPI.parser.on('data', function(frame) {
           console.log ("Records updated!");
         })
       })
+      */
       break; 
   }
   //console.log(formattedDate, '|', deviceID, '>>', deviceVrms);
 });
+app.use(express.static(path.join(__dirname, '/FEcode')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/graph.html');
+  res.sendFile(path.join(__dirname, 'FEcode', 'one.html'));
 });
-
+/*app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/FEcode/one.html');
+});
+*/
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
